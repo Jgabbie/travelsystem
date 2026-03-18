03e90af4997277979ed70cdba13f1e27aa2a003d
Juan Lanuza <jgablanuza@gmail.com>
2026-03-18 22:24:02 +0800

    server changes

--- Backend/server.js
+++ Backend/server.js
@@ -19,10 +19,8 @@ if (!cached) {
 async function connectToDatabase() {
     if (cached.conn) return cached.conn;
     if (!cached.promise) {
-        cached.promise = mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }).then((mongoose) => mongoose);
+        cached.promise = mongoose.connect(process.env.MONGODB_URI) // remove options
+            .then((mongoose) => mongoose);
     }
     cached.conn = await cached.promise;
     return cached.conn;
