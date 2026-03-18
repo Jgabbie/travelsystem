cc2140007dec7471eedbcc4023da9bf75ee5d399
Juan Lanuza <jgablanuza@gmail.com>
2026-03-18 22:11:52 +0800

    server changes

--- Backend/server.js
+++ Backend/server.js
@@ -50,7 +50,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.options('*', cors());
+app.options('/*', cors());
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
