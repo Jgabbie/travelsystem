2000c267a81aaf4374ff973e3b6b4b389e7e71e6
Juan Lanuza <jgablanuza@gmail.com>
2026-04-04 21:32:00 +0800

    uploads fix

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -6,21 +6,21 @@ const path = require('path');
 
 const uploadFolder = path.join(__dirname, '../uploads');
 
-// comment if vercel
-if (!fs.existsSync(uploadFolder)) {
-    fs.mkdirSync(uploadFolder, { recursive: true });
-}
+// // comment if vercel
+// if (!fs.existsSync(uploadFolder)) {
+//     fs.mkdirSync(uploadFolder, { recursive: true });
+// }
 
-// comment if vercel
-const storage = multer.diskStorage({
-    destination: (_req, _file, cb) => {
-        cb(null, uploadFolder);
-    },
-    filename: (_req, file, cb) => {
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-        cb(null, `${uniqueSuffix}-${file.originalname}`);
-    }
-});
+// // comment if vercel
+// const storage = multer.diskStorage({
+//     destination: (_req, _file, cb) => {
+//         cb(null, uploadFolder);
+//     },
+//     filename: (_req, file, cb) => {
+//         const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+//         cb(null, `${uniqueSuffix}-${file.originalname}`);
+//     }
+// });
 
 const upload = multer({ storage });
 
